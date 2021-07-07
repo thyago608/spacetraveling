8685ed6c26ad3304107c8a8f5e97a3735e654fc0
Thyago <thyagoribeiro608@gmail.com>
2021-07-07 17:14:09 -0400

    Passed test header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+
+  const router = useRouter();
+  
+  return(
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <img src="/logo.svg" alt="logo"/>
+        </Link>
+      </div>
+    </header>
+  );
 }
