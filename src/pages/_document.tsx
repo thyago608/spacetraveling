af9b3bda24d287fd6173808907ec9671d8f2e24d
Thyago <thyagoribeiro608@gmail.com>
2021-07-07 12:06:34 -0400

    Added assets, fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,15 @@
-import Document from 'next/document';
+import Document,{Head} from 'next/document';
 
 export default class MyDocument extends Document {
   // render() {
-  //   //TODO
+
   // }
 }
+
+
+/*
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"> 
+
+*/
