e540167e2666a01ca92edfe651f8ee1611f95fda
Thyago <thyagoribeiro608@gmail.com>
2021-07-09 12:28:54 -0400

    Carregamento dos posts do prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import { format, getMonth } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { FiCalendar, FiUser } from 'react-icons/fi';
+
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -27,15 +33,16 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }:HomeProps) {
+
   return(
     <>
       <Head>
          <title> Posts | spacetraveling </title>
       </Head>
       <main className={commonStyles.container}>
-          <div className={styles.postItem}>
-             <Link href="">
+          <div className={styles.post}>
+             <Link href="/post/aijaijs">
                 <a>
                     <strong>Criando um app CRA do zero</strong>
                     <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
@@ -54,7 +61,7 @@ export default function Home() {
              </Link>
           </div>
 
-          <div className={styles.postItem}>
+          <div className={styles.post}>
              <Link href="">
                 <a>
                     <strong>Criando um app CRA do zero</strong>
@@ -80,9 +87,44 @@ export default function Home() {
   );
 }
 
-// export const getStaticProps = async () => {
+export const getStaticProps:GetStaticProps = async () =>{
+  //Iniciando um cliente prismic
+  const prismic = getPrismicClient();
 
-//   const prismic = getPrismicClient();
-//   const response = await prismic.query();
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch:['post.title','post.subtitle','post.author','post.banner','post.content'],
+      pageSize: 3
+    }
+  );
 
-// };
+
+  const posts = response.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(new Date(post.first_publication_date), 'PP',{
+        locale: ptBR
+      }),
+      data:{
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      }
+    }
+  });
+
+
+  const postsPagination = {
+    next_page: response.next_page,
+    results: posts
+  }
+
+
+  return {
+    props:{
+      postsPagination
+    },
+    revalidate: 60 * 30 // 30 minutes
+  }
+}
